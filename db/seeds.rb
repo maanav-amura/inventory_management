0550730d632a24cbc6d19c8bc9199d9ef25559da
maanav-amura <maanav@amuratech.com>
2017-08-29 15:00:44 +0530

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,28 +8,28 @@
 
 #  Console command to load ::  load "#{Rails.root}/db/seeds.rb"
 
-# factory = Factory.create(name: 'aaa', city: 'pune')
-# User::Manager::FactoryManager.create(name: 'aadesh', age: 31, factory_id: factory.id)
-# User::Manager::SalesManager.create(name: 'piyush', age: 21, factory_id: factory.id)
-# User::Manager::InventoryManager.create(name: 'ankush', age: 14, factory_id: factory.id)
-# user = User::Buyer::Customer.new(name:'maanav',age:21)
-# user.factory = factory
-# user.save
-# User::Buyer::Shopkeeper.create(name: 'karan', age: 27, factory_id: factory.id)
-# User::Buyer::Vendor.create(name: 'vikas', age: 47, factory_id: factory.id)
-# Product.create(name:'tango',price:125,capacity:100,available: true,factory_id: factory.id)
-# Product.create(name:'clarity',price:50,capacity:100,available: true,factory_id: factory.id)
-# customer = User.find_by_type('User::Buyer::Customer')
-# bill = InvoiceBill.new(total_price:175)
-# bill.user = customer
-# bill.save
-# bill.invoice_details << InvoiceDetail.new(product_id: Product.first.id, quantity:1)
-# bill.invoice_details << InvoiceDetail.new(product_id: Product.second.id, quantity:1)
-# bill.products
-# bill.invoice_details
-# u = User.find_by_type('User::Manager::InventoryManager')
-# p = Product.first
-# u.products << p
-# p  = Product.first
-# p.managers
-# u.products
+factory = Factory.create(name: 'aaa', city: 'pune')
+User::Manager::FactoryManager.create(name: 'maanav', age: 31, factory_id: factory.id, email: 'maanav@gmail.com', password: 'maanavshah')
+User::Manager::SalesManager.create(name: 'piyush', age: 21, factory_id: factory.id, email: 'piyush@gmail.com', password: 'piyushwani')
+User::Manager::InventoryManager.create(name: 'ankush', age: 14, factory_id: factory.id, email: 'ankush@gmail.com', password: 'ankushkhurrana')
+user = User::Buyer::Customer.new(name:'aadesh',age:21, email: 'aadesh@gmail.com', password: 'aadeshshah')
+user.factory = factory
+user.save
+User::Buyer::Shopkeeper.create(name: 'karan', age: 27, factory_id: factory.id, email: 'karan@gmail.com', password: 'karantongay')
+User::Buyer::Vendor.create(name: 'vikas', age: 47, factory_id: factory.id, email: 'vikas@gmail.com', password: 'vikasprasad')
+Product.create(name:'tango',price:125,capacity:100,available: true,factory_id: factory.id)
+Product.create(name:'clarity',price:50,capacity:100,available: true,factory_id: factory.id)
+customer = User.find_by_type('User::Buyer::Customer')
+bill = InvoiceBill.new(total_price:175)
+bill.user = customer
+bill.save
+bill.invoice_details << InvoiceDetail.new(product_id: Product.first.id, quantity:1)
+bill.invoice_details << InvoiceDetail.new(product_id: Product.second.id, quantity:1)
+bill.products
+bill.invoice_details
+u = User.find_by_type('User::Manager::InventoryManager')
+p = Product.first
+u.products << p
+p  = Product.first
+p.managers
+u.products
