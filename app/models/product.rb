9070bae3cbb401f0eae03be1e8d1baa05c3889b7
maanav-amura <maanav@amuratech.com>
2017-08-31 13:38:37 +0530

    adding scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,7 @@ class Product < ApplicationRecord
   has_many :invoice_details
   has_many :invoice_bills, through: :invoice_details
   belongs_to :factory
+
+  scope :all_products, -> { order('name ASC') }
+  scope :available_products, -> { where(available: true).order('name ASC') }
 end
