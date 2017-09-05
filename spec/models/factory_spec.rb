e07f47909fa6baf1f377804602492630d079b572
maanav-amura <maanav@amuratech.com>
2017-09-05 21:10:58 +0530

    adding rspec to models

--- spec/models/factory_spec.rb
+++ spec/models/factory_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Factory, type: :model do
-  describe 'valid factories' do
-    let(:factory) { Factory.new(name: 'dota') }
+  describe 'Valid Factory' do
+    before(:each) do
+      @factory = Factory.new(name: 'Dota')
+    end
 
     describe 'Associations' do
       it { should have_many(:products) }
@@ -13,15 +15,36 @@ RSpec.describe Factory, type: :model do
     describe 'Validations' do
       it { should validate_presence_of(:name) }
     end
+
+    describe Factory.new(name: 'Dota') do
+      it { is_expected.to have_attributes(name: 'Dota') }
+    end
+
+    it 'is not valid if it is not an instance of Factory' do
+      expect(@factory.instance_of?(Factory)).to be
+    end
+
+    it 'is not valid if its name not a String' do
+      expect(@factory.name.class).to match(String)
+    end
   end
 
-  describe 'invalid factories' do
-    before(:each) do
-      @factory = Factory.new
-    end
+  describe 'Invalid Factory' do
+    let(:factory) { Factory.new }
 
     it 'is not valid without a valid name' do
-      expect(@factory).to_not be_valid
+      expect(factory).to_not be_valid
     end
+
+    it 'is not valid if it is not an instance of Factory' do
+      factory = User.new
+      expect(factory.instance_of?(Factory)).to_not be
+    end
+
+    it 'is not valid if its name not a String' do
+      factory.name = 123
+      expect(factory.name.class).to be(String)
+    end
+
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,21 +14,39 @@ RSpec.describe Product, type: :model do
       it { should validate_numericality_of(:price) }
       it { should validate_numericality_of(:capacity) }
     end
+
+    describe Product.new(name: 'desolator', price: 350, capacity: 100, available: true, factory_id: Factory.first.id) do
+      it { is_expected.to have_attributes(name: 'desolator') }
+      it { is_expected.to have_attributes(price: 350) }
+      it { is_expected.to have_attributes(capacity: 100) }
+      it { is_expected.to have_attributes(available: true) }
+    end
+
+    it 'is not valid if it is not an instance of Factory' do
+      expect(product.instance_of?(Product)).to be
+    end
+
+    it 'is not valid if its name not a String' do
+      expect(product.name.class).to match(String)
+    end
   end
 
   describe 'invalid products' do
     let(:product) { Product.new(price: '350') }
 
-    it 'is not valid without a valid name' do
-      expect(product).to_not be_valid
+    describe Product.new do
+      it { is_expected.to_not have_attributes(name: 'desolator') }
+      it { is_expected.to_not have_attributes(price: 350) }
+      it { is_expected.to_not have_attributes(capacity: 100) }
+      it { is_expected.to_not have_attributes(available: true) }
     end
 
-    it 'is not valid without a valid and numeric price' do
-      expect(product).to_not be_valid
+    it 'is not valid if it is not an instance of Factory' do
+      expect(product.instance_of?(User)).to_not be
     end
 
-    it 'is not valid without a valid and numeric capacity' do
-      expect(product).to_not be_valid
+    it 'is not valid if its name not a String' do
+      expect(product.name.class).to_not be(Integer)
     end
   end
 end
