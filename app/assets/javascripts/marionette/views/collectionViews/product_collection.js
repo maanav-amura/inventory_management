01e8fb0e048409e3a1cb10a951d04d228afee494
maanav-amura <maanav@amuratech.com>
2017-09-27 17:46:27 +0530

    updating to render composite view in layout

--- app/assets/javascripts/marionette/views/collectionViews/product_collection.js
+++ app/assets/javascripts/marionette/views/collectionViews/product_collection.js
@@ -1,4 +1,3 @@
 var ProductCollection = Marionette.CollectionView.extend({
   childView: ProductItem,
-  tagName: 'tbody'
 });
--- app/assets/javascripts/marionette/views/itemViews/product_item.js
+++ app/assets/javascripts/marionette/views/itemViews/product_item.js
@@ -1,22 +1,28 @@
 var ProductItem = Marionette.ItemView.extend({
+  template: JST['product_item'],
+
   tagName: 'tr',
+
   events: {
     'click .show': 'productShow',
     'click .edit' : 'productEdit',
     'click .remove': 'productRemove'
   },
-  template: JST['product_item'],
+
   serializeData: function() {
     return {
       'products' : this.model.attributes
     }
   },
+
   productShow: function(e) {
     Backbone.history.navigate(`products/${$(e.currentTarget).data("id")}`, {trigger: true, replace: true});
   },
-    productEdit: function(e) {
+
+  productEdit: function(e) {
     Backbone.history.navigate(`products/${$(e.currentTarget).data("id")}/edit`, {trigger: true, replace: true});
   },
+
   productRemove: function(e) {
     this.model.destroy({
       success: function(result) {
@@ -24,18 +30,23 @@ var ProductItem = Marionette.ItemView.extend({
       }
     });
   },
+
   onBeforeRender: function() {
     console.log('before render');
   },
+
   onRender: function() {
     console.log('on render');
   },
+
   onBeforeDestroy: function() {
     console.log('before destroy');
   },
+
   onDestroy: function() {
     console.log('on destroy');
   },
+
   onAttach: function() {
     console.log('on attach')
   }
