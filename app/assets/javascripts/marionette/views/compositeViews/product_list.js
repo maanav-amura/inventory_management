61098520690b77527b3756df8d89b19c24a72853
maanav-amura <maanav@amuratech.com>
2017-09-28 17:49:31 +0530

    added search using ajax for products

--- app/assets/javascripts/marionette/views/compositeViews/product_list.js
+++ app/assets/javascripts/marionette/views/compositeViews/product_list.js
@@ -3,9 +3,24 @@ var ProductList = Marionette.CompositeView.extend({
   childView: ProductItem,
   childViewContainer: 'tbody',
     events: {
-    'click .new': 'new',
+    'click .new ': 'new',
+    'keyup #search' : 'search'
   },
   new: function() {
     Backbone.history.navigate('products/new',{trigger: true});
+  },
+  search: function() {
+    $.ajax({
+      type: 'GET',
+      url: '/products/search',
+      data: { search : $('#search').val() },
+      dataType: 'json',
+      success: function(response) {
+        $('#products').text("");
+        response.forEach(function(product) {
+          $('#products').append('<option value="' + product.name + '">');
+        })
+      }
+    });
   }
 });
